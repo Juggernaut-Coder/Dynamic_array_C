f88985f47088eed580468231607a77ab3d96a057
Dirty Void <dirtyvoid@skiff.com>
2023-01-06 04:03:56 -0400

    Intial release

--- src_headers/vector.h
+++ src_headers/vector.h
@@ -244,6 +244,10 @@ static inline int _vec_insert(void **data, size_t *len, size_t *cap, size_t mems
     qsort( vdat(v), vlen(v), vdsize(v), (sort_fn) )
 
 /* Vector Clone */
+#define vec_clone(v, vcpy)              \
+    vec_init( (vcpy) , vlen(v) )        \
+    memcpy(*vcpy, *v, vsize(v))         \
+    vcap(vcpy) = vlen(v)            
 
 /** 
  * Some for loop iteration schemes are provided
